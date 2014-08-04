32253c40b68d1c4283685de8c1e5f02188a76af7
brijeshmeshiya <meshiya_brijesh@yahoo.com>
2014-08-04 17:40:05 +0530

    Minor bug related to NULL pointer is fixed

--- gui.java
+++ gui.java
@@ -10,8 +10,8 @@ import java.awt.event.MouseEvent;
 
 
 public class gui {
-	public gui(){
 		JLabel[][] cell = new JLabel[4][4];
+	public gui(){
 		JFrame board = new JFrame();
 		board.setResizable(false);
 		board.getContentPane().setBackground(Color.WHITE);
@@ -19,86 +19,102 @@ public class gui {
 		board.setSize(355,425);
 		board.getContentPane().setLayout(null);
 		
-		cell[0][0].setText("1");
+		cell[0][0] = new JLabel("1");
 		cell[0][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][0].setBounds(12, 12, 84, 84);
 		board.getContentPane().add(cell[0][0]);
 		
+		cell[0][1] = new JLabel("1");
 		cell[0][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][1].setBounds(94, 12, 84, 84);
 		board.getContentPane().add(cell[0][1]);
 		
+		cell[0][2] = new JLabel("1");
 		cell[0][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][2].setBounds(176, 12, 84, 84);
 		board.getContentPane().add(cell[0][2]);
 		
+		cell[0][3] = new JLabel("1");
 		cell[0][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][3].setBounds(258, 12, 84, 84);
 		board.getContentPane().add(cell[0][3]);
 		
+		cell[1][0] = new JLabel("1");
 		cell[1][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[1][0].setBounds(258, 94, 84, 84);
 		board.getContentPane().add(cell[1][0]);
 		
+		cell[1][1] = new JLabel("1");
 		cell[1][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[1][1].setBounds(176, 94, 84, 84);
 		board.getContentPane().add(cell[1][1]);
 		
+		cell[1][2] = new JLabel("1");
 		cell[1][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[1][2].setBounds(94, 94, 84, 84);
 		board.getContentPane().add(cell[1][2]);
 		
+		cell[1][3] = new JLabel("1");
 		cell[1][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[1][3].setBounds(12, 94, 84, 84);
 		board.getContentPane().add(cell[1][3]);
 		
+		cell[2][0] = new JLabel("1");
 		cell[2][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[2][0].setBounds(258, 176, 84, 84);
 		board.getContentPane().add(cell[2][0]);
 		
+		cell[2][1] = new JLabel("1");
 		cell[2][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[2][1].setBounds(176, 176, 84, 84);
 		board.getContentPane().add(cell[2][1]);
 		
+		cell[2][2] = new JLabel("1");
 		cell[2][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[2][2].setBounds(94, 176, 84, 84);
 		board.getContentPane().add(cell[2][2]);
 		
+		cell[2][3] = new JLabel("1");
 		cell[2][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[2][3].setBounds(12, 176, 84, 84);
 		board.getContentPane().add(cell[2][3]);
 		
+		cell[3][0] = new JLabel("1");
 		cell[3][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[3][0].setBounds(258, 258, 84, 84);
 		board.getContentPane().add(cell[3][0]);
 		
+		cell[3][1] = new JLabel("1");
 		cell[3][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[3][1].setBounds(176, 258, 84, 84);
 		board.getContentPane().add(cell[3][1]);
 		
+		cell[3][2] = new JLabel("1");
 		cell[3][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[3][2].setBounds(94, 258, 84, 84);
 		board.getContentPane().add(cell[3][2]);
 		
+		cell[3][3] = new JLabel("1");
+		//JLabel label_14 = new JLabel("1");
 		cell[3][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[3][3].setBounds(12, 258, 84, 84);
-		board.getContentPane().add(cell[3][3]);
+		board.add(cell[3][3]);
 		
 		final Button button = new Button("Start");
 		button.setName("Start");
