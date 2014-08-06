232e207cc3c0038d6ec562a56cd29d71f93424c4
brijeshmeshiya <meshiya_brijesh@yahoo.com>
2014-08-07 02:59:13 +0530

    Bug with cell Arrangement is Fixed

--- gui.java
+++ gui.java
@@ -26,99 +26,111 @@ public class gui {
 		cell[0][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][0].setBounds(12, 12, 84, 84);
-		board.getContentPane().add(cell[0][0]);
+		//board.getContentPane().add(cell[0][0]);
 		
 		cell[0][1] = new JLabel("");
 		cell[0][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][1].setBounds(94, 12, 84, 84);
-		board.getContentPane().add(cell[0][1]);
+		//board.getContentPane().add(cell[0][1]);
 		
 		cell[0][2] = new JLabel("");
 		cell[0][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][2].setBounds(176, 12, 84, 84);
-		board.getContentPane().add(cell[0][2]);
+	//	board.getContentPane().add(cell[0][2]);
 		
 		cell[0][3] = new JLabel("");
 		cell[0][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[0][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		cell[0][3].setBounds(258, 12, 84, 84);
-		board.getContentPane().add(cell[0][3]);
+		//board.getContentPane().add(cell[0][3]);
 		
 		cell[1][0] = new JLabel("");
 		cell[1][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[1][0].setBounds(258, 94, 84, 84);
-		board.getContentPane().add(cell[1][0]);
+	//	board.getContentPane().add(cell[1][0]);
 		
 		cell[1][1] = new JLabel("");
 		cell[1][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[1][1].setBounds(176, 94, 84, 84);
-		board.getContentPane().add(cell[1][1]);
+		//board.getContentPane().add(cell[1][1]);
 		
 		cell[1][2] = new JLabel("");
 		cell[1][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[1][2].setBounds(94, 94, 84, 84);
-		board.getContentPane().add(cell[1][2]);
+		//board.getContentPane().add(cell[1][2]);
 		
 		cell[1][3] = new JLabel("");
 		cell[1][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[1][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[1][3].setBounds(12, 94, 84, 84);
-		board.getContentPane().add(cell[1][3]);
+		
+		//board.getContentPane().add(cell[1][3]);
+		
+		cell[1][3].setBounds(258, 94, 84, 84);
+		cell[1][0].setBounds(12, 94, 84, 84);
+		cell[1][1].setBounds(94, 94, 84, 84);
+		cell[1][2].setBounds(176, 94, 84, 84);
 		
 		cell[2][0] = new JLabel("");
 		cell[2][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[2][0].setBounds(258, 176, 84, 84);
-		board.getContentPane().add(cell[2][0]);
+		//board.getContentPane().add(cell[2][0]);
 		
 		cell[2][1] = new JLabel("");
 		cell[2][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[2][1].setBounds(176, 176, 84, 84);
-		board.getContentPane().add(cell[2][1]);
+		//board.getContentPane().add(cell[2][1]);
 		
 		cell[2][2] = new JLabel("");
 		cell[2][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[2][2].setBounds(94, 176, 84, 84);
-		board.getContentPane().add(cell[2][2]);
+		//board.getContentPane().add(cell[2][2]);
 		
 		cell[2][3] = new JLabel("");
 		cell[2][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[2][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[2][3].setBounds(12, 176, 84, 84);
-		board.getContentPane().add(cell[2][3]);
+		//board.getContentPane().add(cell[2][3]);
+		
+		cell[2][3].setBounds(258, 176, 84, 84);
+		cell[2][2].setBounds(176, 176, 84, 84);
+		cell[2][1].setBounds(94, 176, 84, 84);
+		cell[2][0].setBounds(12, 176, 84, 84);
+		
 		
 		cell[3][0] = new JLabel("");
 		cell[3][0].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[3][0].setBounds(258, 258, 84, 84);
-		board.getContentPane().add(cell[3][0]);
+		//board.getContentPane().add(cell[3][0]);
 		
 		cell[3][1] = new JLabel("");
 		cell[3][1].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[3][1].setBounds(176, 258, 84, 84);
-		board.getContentPane().add(cell[3][1]);
+		//board.getContentPane().add(cell[3][1]);
 		
 		cell[3][2] = new JLabel("");
 		cell[3][2].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[3][2].setBounds(94, 258, 84, 84);
-		board.getContentPane().add(cell[3][2]);
+		//board.getContentPane().add(cell[3][2]);
 		
 		cell[3][3] = new JLabel("");
 		cell[3][3].setHorizontalAlignment(SwingConstants.CENTER);
 		cell[3][3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		cell[3][3].setBounds(12, 258, 84, 84);
 		cell[3][3].setBackground(Color.GREEN);
-		board.getContentPane().add(cell[3][3]);
+		//board.getContentPane().add(cell[3][3]);
 		
+		cell[3][3].setBounds(258, 258, 84, 84);
+		cell[3][2].setBounds(176, 258, 84, 84);
+		cell[3][1].setBounds(94, 258, 84, 84);
+		cell[3][0].setBounds(12, 258, 84, 84);
+		
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				board.getContentPane().add(cell[i][j]);
+			}
+		}
 		
 		final Button button = new Button("Start");
 		button.setName("Start");
